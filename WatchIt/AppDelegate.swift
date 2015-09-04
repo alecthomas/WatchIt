46e3f363d6a9dc4986bd36b5064be48e1705d72c
Alec Thomas <alec@swapoff.org>
2015-09-04 18:18:09 +1000

    Initial Commit

---
+++ WatchIt/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WatchIt
+//
+//  Created by Alec Thomas on 4/09/2015.
+//  Copyright © 2015 SwapOff. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
